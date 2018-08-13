3b011c431dc5668f86bc7a05113ff51b24d81850
Andrew McClain <awmcclain@gmail.com>
2018-08-13 11:40:00 -0700

    Updated about screen

--- app/renderer/about/controller.js
+++ app/renderer/about/controller.js
@@ -3,7 +3,8 @@ const ipc = electron.ipcRenderer;
 const $ = window.jQuery = require('../jquery-2.2.3.min.js');
 
 ipc.on("set-about-data", (event, data) => {
-    $("#version-inky").text("Inky version: "+data.inkyVersion);
+    //$("#version-mcslinky").text(data);
+    $("#version-inky").text("McSlinky version: "+data.inkyVersion);
     $("#version-ink").text("ink version: "+data.inkVersion);
     $("#version-inkjs").text("inkjs version: "+data.inkjsVersion);
 });
